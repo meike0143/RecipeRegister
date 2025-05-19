6fa49b447ab8e732227d7ebd00d4e0db57b1569e
meike <mart0143@hz.nl>
2025-05-19 09:57:52 +0200

    Implemented CRUD

--- resources/views/components/recipe/list-item.blade.php
+++ resources/views/components/recipe/list-item.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+<div class="bg-gray-100 border-gray-200 border-4 shadow-sm sm:rounded-lg my-2">
+    <div class="p-6 text-gray-900">
+        <div class="sm:items-center">
+            <a class="font-semibold text-l text-gray-800" href="{{ route('recipes.show', $recipe) }}">
+                {{ $recipe->name }}
+            </a>
+        </div>
+    </div>
 </div>
